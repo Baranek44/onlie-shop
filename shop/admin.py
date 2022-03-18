c181bea2d26248c0d5d1ed05375af49e649a1696
Mateusz Baranowski <baku13681@gmail.com>
2022-03-19 00:16:29 +0100

    first commit

---
+++ shop/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from.models import Category, Product
+
+# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated']
+    list_editable = ['price', 'available']
+    # Specify fields where the value is automatically set using the value of other fields
+    prepopulated_fields = {'slug': ('name',)}
