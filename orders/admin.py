22044b1e68b8a94353768fb73b5c3db032b42912
Mateusz Baranowski <baku13681@gmail.com>
2022-03-26 11:54:04 +0100

    Add few options to orders

--- orders/admin.py
+++ orders/admin.py
@@ -1,11 +1,54 @@
 from django.contrib import admin
 from .models import Order, OrderItem
+import csv
+import datetime
+from django.http import HttpResponse
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+
 
 # Register your models here.
-
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
+
+
+"""
+Create a custom administration action to download a list of orders as CSV file.
+"""
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    content_disposition = 'attachment; filename={opts.verbose_name}.csv'
+    response = HttpResponse(content_type="text/csv")
+    response['Content-Disposition'] = content_disposition
+    writer = csv.writer(response)
+
+    fields = [field for field in opts.get_fields() if not \
+        field.many_to_many and not field.one_to_many] 
+    #Here is a first row information with header    
+    writer.writerow([field.verbose_name for field in fields])
+    # Write rows data
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+export_to_csv.short_description = 'Export to CSV'
+
+
+# Display page of the administration site
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
+
+def order_pdf(obj):
+    url = reverse('orders:admin_order_pdf', args=[obj.id])
+    return mark_safe(f'<a href="{url}">PDF</a>')
+order_pdf.short_description = 'Invoice'
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
@@ -19,10 +62,14 @@ class OrderAdmin(admin.ModelAdmin):
         'city',
         'paid',
         'created',
-        'updated']
+        'updated',
+        order_detail,
+        order_pdf]
     list_filter = [
         'paid',
         'created',
         'updated']
     inlines = [OrderItemInline]
+    # generic administration action that can be added to any ModelAdmin class
+    actions = [export_to_csv]
 
--- orders/views.py
+++ orders/views.py
@@ -1,15 +1,21 @@
-from django.shortcuts import render, redirect
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import render, redirect, get_object_or_404
+from django.template.loader import render_to_string
+from django.conf import settings
+from django.http import HttpResponse
 from django.urls import reverse
-from .models import OrderItem
-from .forms import OrderCreateForm
 from cart.cart import Cart
+from .models import OrderItem, Order
+from .forms import OrderCreateForm
 from .tasks import order_created
+import weasyprint
+
 
 # Create your views here.
+
 """
 From to create new Order objects, thsi view handle the form and create a new order
 """
-
 def order_create(request):
     cart = Cart(request)
     if request.method == 'POST':
@@ -39,3 +45,31 @@ def order_create(request):
         {'cart': cart, 'form': form}
     )
 
+
+"""
+Display information about an order
+staff_member_required just decorates checks for is_active and is_staff fields 
+"""
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(
+        request,
+        'admin/orders/order/detail.html',
+        {'order': order})
+
+"""
+Create simple view to generate PDF invoices for existing orders
+staff_member_required is for decorator to make sure only staff users can access this view
+"""
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    html = render_to_string('orders/order/pdf.html',
+                            {'order': order})
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'filename=order_{order.id}.pdf'
+    weasyprint.HTML(string=html).write_pdf(response,
+        stylesheets=[weasyprint.CSS(
+            settings.STATIC_ROOT + 'css/pdf.css')])
+    return response
