6d86996ff7a108cefb6306a0e3ea43e405fdf38d
Mateusz Baranowski <baku13681@gmail.com>
2022-03-28 16:51:04 +0200

    Add session coupon_id

--- cart/cart.py
+++ cart/cart.py
@@ -2,6 +2,7 @@ from decimal import Decimal
 from itertools import product
 from django.conf import settings
 from shop.models import Product 
+from coupons.models import Coupon
 
 class Cart(object):
 
@@ -15,6 +16,8 @@ class Cart(object):
             # save an empty cart in the session
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
+        # store current applied coupon
+        self.coupon_id = self.session.get('coupon_id')
 
     def add(self, product, quantity=1, override_quantity=False):
         """
@@ -75,3 +78,25 @@ class Cart(object):
         # remove cart from session
         del self.session[settings.CART_SESSION_ID]
         self.save()
+
+    ## Get the coupon_id session key from the current session
+    @property
+    def coupon(self):
+        # Define this method as a property. If the cart contains 
+        # a coupon_id attribute, the Coupon object with the given ID is returned
+        if self.coupon_id:
+            try:
+                return Coupon.objects.get(id=self.coupon_id)
+            except Coupon.DoesNotExist:
+                pass
+        return  None
+        
+    def get_discount(self):
+        # If the cart contains a coupon, you retrieve its discount 
+        # rate and return the amount to be deducted from the total amount of the cart.
+        if self.coupon:
+            return (self.coupon.discount / Decimal(100)) * self.get_total_price()
+        return Decimal(0)
+
+    def get_total_price_after_discount(self):
+        return self.get_total_price() - self.get_discount()
